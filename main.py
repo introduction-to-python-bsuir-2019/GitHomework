dec9424b5ad41077e462ff144138948c61833561
Klimets <artem.klimec8@gmail.com>
2019-10-17 16:06:37 +0300

    owner_name
    Added the ability to enter some name and the script will respond with a greeting using the entered name. And after this print a question "How is the weather today?"

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello brave new world")
+while True:
+	name = input("please enter your name: ")
+	if name.isalpha():
+		break
+	else:
+		print("Invalid name")
+print(f"Hello {name}")
+print("How is the weather today?")
